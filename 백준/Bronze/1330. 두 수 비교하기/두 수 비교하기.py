b24dc50038a28c98af528e81d6cbbacad063cc63
jhjreally <137694005+jhjhj0516@users.noreply.github.com>
2023-07-01 18:22:22 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.py
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.py
@@ -1,8 +1,7 @@
-a, b = map(int, input().split())
-
-if a > b :
-    print('>')
-elif a < b :
-    print('<')
+A, B = map(int, (input().split()))
+if A > B :
+    print(">")
+elif A < B :
+    print("<")
 else :
-    print('==')
+    print("==")
