dd41a047cc31519e3edf4aa4a5df4cedc29c48cb
jhjreally <137694005+jhjhj0516@users.noreply.github.com>
2023-08-05 00:46:45 +0900

    [Bronze II] Title: 수 정렬하기, Time: 84 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,10 +1,21 @@
-arr = []
+import sys
 
-n = int(input())
-for _ in range(n) :
-  num = int(input())
-  arr.append(num)
+N = int(sys.stdin.readline())
+num_list = []
+new_list = []
 
-arr.sort()
-for i in range(n) :
-  print(arr[i])
+for i in range(N) :
+  num_list.append(int(sys.stdin.readline()))
+    
+for i in range(0, N) :
+  if len(new_list) == 0 or new_list[-1] < num_list[i]:
+    new_list.append(num_list[i])
+    continue
+
+  for k in range(0, len(new_list)) :
+    if num_list[i] < new_list[k] :
+      new_list.insert(k, num_list[i])
+      break
+        
+for num in new_list :
+  print(num)
